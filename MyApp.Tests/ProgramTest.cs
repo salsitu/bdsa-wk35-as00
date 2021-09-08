3da06e0413d81a88c084128b0685313637281a18
salsitu <90044151+salsitu@users.noreply.github.com>
2021-09-08 14:49:54 +0200

    Tested and implemented isLeapYear(). (#3)

---
+++ MyApp.Tests/ProgramTest.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace MyApp.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void isLeapYearTest()
+        {
+            Assert.True(Program.isLeapYear(2004));
+            Assert.False(Program.isLeapYear(1900));
+            Assert.True(Program.isLeapYear(1600));
+        }
+    }
+}

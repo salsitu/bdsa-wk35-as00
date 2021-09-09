260d86702b2345747076853bfdb52f3295435488
salsitu <90044151+salsitu@users.noreply.github.com>
2021-09-09 17:37:50 +0200

    Feature/exercises (#4)
    * Tested and implemented user input.
    
    * Implemented and tested error handling.
    
    * Added documentation.

--- MyApp.Tests/ProgramTest.cs
+++ MyApp.Tests/ProgramTest.cs
@@ -1,10 +1,75 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace MyApp.Tests
 {
     public class ProgramTests
     {
+        [Fact]
+        public void MainPrintsYay()
+        {
+            StringReader reader = new StringReader("2000");
+            Console.SetIn(reader);
+            StringWriter writer = new StringWriter();
+            Console.SetOut(writer);
+
+            Program.Main();
+
+            string expected = "yay";
+            string actual = writer.ToString().Split(Environment.NewLine)[1];
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MainPrintsNay()
+        {
+            StringReader reader = new StringReader("1900");
+            Console.SetIn(reader);
+            StringWriter writer = new StringWriter();
+            Console.SetOut(writer);
+
+            Program.Main();
+
+            string expected = "nay";
+            string actual = writer.ToString().Split(Environment.NewLine)[1];
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MainNotANumber()
+        {
+            StringReader reader = new StringReader("190d");
+            Console.SetIn(reader);
+            StringWriter writer = new StringWriter();
+            Console.SetOut(writer);
+
+            Program.Main();
+
+            string expected = "You need to enter a number, quitting!";
+            string actual = writer.ToString().Split(Environment.NewLine)[1];
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void MainBelow1582()
+        {
+            StringReader reader = new StringReader("1581");
+            Console.SetIn(reader);
+            StringWriter writer = new StringWriter();
+            Console.SetOut(writer);
+
+            Program.Main();
+
+            string expected = "Cannot handle years below 1582, quitting!";
+            string actual = writer.ToString().Split(Environment.NewLine)[1];
+
+            Assert.Equal(expected, actual);
+        }
+
         [Fact]
         public void isLeapYearTest()
         {
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -4,14 +4,41 @@ namespace MyApp
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Type a year:");
+            string input = Console.ReadLine();
+            int parsed;
+            
+            try
+            {
+                 parsed = int.Parse(input);
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("You need to enter a number, quitting!");
+                return;
+            }
+
+            if (parsed < 1582)
+            {
+                Console.WriteLine("Cannot handle years below 1582, quitting!");
+                return;
+            }
+            
+            if (isLeapYear(parsed))
+            {
+                Console.WriteLine("yay");
+            }
+            else
+            {
+                Console.WriteLine("nay");
+            }
         }
 
         public static bool isLeapYear(int year)
         {
-            return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+            return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
         }
     }
 }

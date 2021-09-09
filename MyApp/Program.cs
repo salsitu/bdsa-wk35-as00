f6ab374bcbf83f998a366317f5e5a9746fbe251c
salsitu <sals@itu.dk>
2021-09-09 17:08:37 +0200

    Swapped statements in isLeapYear().

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -38,7 +38,7 @@ namespace MyApp
 
         public static bool isLeapYear(int year)
         {
-            return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+            return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
         }
     }
 }
